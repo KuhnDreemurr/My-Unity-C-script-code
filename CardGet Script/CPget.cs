b7bec8ff3b97060a43c6779328c65d1d6f625276
KuhnDreemurr <79293577+KuhnDreemurr@users.noreply.github.com>
2023-05-18 01:07:38 +0800

    Add files via upload
    抽卡DEMO腳本

---
+++ CardGet Script/CPget.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CPget : MonoBehaviour {
+    public int GC;//機率變數
+    public Button Go;//抽卡按鈕
+    public List<int> cn = new List<int>(); //機率結果儲存
+    public List<string> GR = new List<string>(); //得到的R卡紀錄
+    public List<string> GSR = new List<string>();
+    public List<string> GSSR = new List<string>();
+    public List<string> GN = new List<string>();
+    //public GameObject Book; //按鈕激活
+    public GameObject[] DrawPre;//確保本體不滅，抽卡影片克隆
+    public GameObject[] R_elf;
+    public GameObject[] SR_elf;
+    public GameObject[] SSR_elf;
+    public GameObject[] N_elf;
+    public int VVID; //要調用的本體陣列第幾位
+    public GameObject Draw; //抽卡影片本體
+    public GameObject VC; //防止影片在背景撥放
+    public int CG;//計算單一抽卡list次數(調用抽到第幾張)
+    public int allCG;//計算抽卡累積次數(將來可作保底)
+    public GameObject Cardshow;
+    public GameObject FirstV;
+    public Button Search;//查詢紀錄按鈕
+    public string ListData; //儲存抽卡紀錄
+    // Use this for initialization
+    void Start() {
+        CG = 0;
+        if (PlayerPrefs.HasKey("抽卡次數"))
+        {
+            allCG = PlayerPrefs.GetInt("抽卡次數");
+        }
+        else allCG = 0;
+
+        if (PlayerPrefs.HasKey("抽卡紀錄"))
+        {
+            ListData = PlayerPrefs.GetString("抽卡紀錄");
+        }
+        else ListData = "";
+        StartCoroutine(CoNov());
+        
+        Go.onClick.AddListener(delegate ()
+        {
+            for (int t = 1; t <=6; t++) {
+                GC = 100 - (Random.Range(0, 100));
+                Debug.Log(GC);
+                cn.Add(GC);
+                ListData += GC.ToString()+",";
+                PlayerPrefs.SetString("抽卡紀錄",ListData);
+                PlayerPrefs.Save();
+                Debug.Log("資料-全部 存檔完成");
+
+            }
+            
+            VC.gameObject.active = true;
+            Draw = Instantiate(DrawPre[VVID], new Vector3(960, 540, 0), Quaternion.Euler(new Vector3(0, 0, 0)), FirstV.transform);
+            //Draw.transform.position = new Vector3(960, 540, 0);
+            StartCoroutine(CoVideo());
+            //DestroyImmediate(Draw, true);
+        });
+        
+        Search.onClick.AddListener(delegate ()
+        {
+            Debug.Log("Enter");
+            if (PlayerPrefs.HasKey("抽卡次數"))
+            {
+                Debug.Log("Yes");
+                Debug.Log(PlayerPrefs.GetInt("抽卡次數"));               
+            }
+            if (PlayerPrefs.HasKey("抽卡紀錄"))
+            {
+                Debug.Log("Yoo");
+                Debug.Log(PlayerPrefs.GetString("抽卡紀錄"));
+
+                //卡冊會用到，讀取拆解抽卡紀錄資料，讀取數字並重新克隆該數字的卡片
+                string[] A = PlayerPrefs.GetString("抽卡紀錄").Split(char.Parse(","));
+                for (int i = 0; i < A.Length; i++)
+                {
+                    Debug.Log(A[i]);
+                }
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerPrefs.DeleteKey("抽卡次數");
+            Debug.Log("資料-抽卡次數 刪除完成");
+            allCG = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            PlayerPrefs.DeleteKey("抽卡紀錄");
+            Debug.Log("資料-抽卡紀錄 刪除完成");
+            ListData = "";
+        }
+    }
+    IEnumerator CoNov()
+    {
+        yield return new WaitForSeconds(6);
+        Go.gameObject.active = true;
+        Search.gameObject.active = true;
+    }
+    IEnumerator CoVideo()
+    {
+        Go.gameObject.active = false;
+        Search.gameObject.active = false;
+        //Book.active = false;
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第一張");
+        CardBool();
+        yield return new WaitForSeconds(2.2f);
+        Debug.Log("第一張消失");
+        Destroy(Cardshow);
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第二張");
+        CardBool();
+        yield return new WaitForSeconds(2.2f);
+        Debug.Log("第二張消失");
+        Destroy(Cardshow);
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第三張");
+        CardBool();
+        yield return new WaitForSeconds(2.2f);
+        Debug.Log("第三張消失");
+        Destroy(Cardshow);
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第四張");
+        CardBool();
+        yield return new WaitForSeconds(2.2f);
+        Debug.Log("第四張消失");
+        Destroy(Cardshow);
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第五張");
+        CardBool();
+        yield return new WaitForSeconds(2.2f);
+        Debug.Log("第五張消失");
+        Destroy(Cardshow);
+        yield return new WaitForSeconds(3.3f);
+        Debug.Log("第六張");
+        CardBool();
+        yield return new WaitForSeconds(2f);
+        Debug.Log("第六張消失");
+        Destroy(Cardshow);
+        //Book.active = true;
+        CG = 0;
+        Go.gameObject.active = true;
+        Search.gameObject.active = true;
+        yield return new WaitForSeconds(0.2f);
+        Destroy(Draw);
+        VC.gameObject.active = false;
+        Debug.Log("DD");
+    }
+
+    /// <summary>
+    /// 抽卡稀有度判定
+    /// </summary>
+    public void CardBool()
+    {
+        Debug.Log(CG);
+        if (cn[CG] >= 1 && cn[CG] <= 5)
+        {
+            Cardshow = Instantiate(SSR_elf[Random.Range(0, 2)], new Vector3(960, 540, 0), Quaternion.Euler(new Vector3(0, 0, 0)), Draw.transform);
+        }
+        else if (cn[CG] >= 6 && cn[CG] <= 20)
+        {
+            Cardshow = Instantiate(SR_elf[Random.Range(0, 3)], new Vector3(960, 540, 0), Quaternion.Euler(new Vector3(0, 0, 0)), Draw.transform);
+        }
+        else if (cn[CG] >= 21 && cn[CG] <= 50)
+        {
+            Cardshow = Instantiate(R_elf[Random.Range(0, 3)], new Vector3(960, 540, 0), Quaternion.Euler(new Vector3(0, 0, 0)), Draw.transform);
+        }
+        else if (cn[CG] >= 51 && cn[CG] <= 100)
+        {
+            Cardshow = Instantiate(N_elf[Random.Range(0, 7)], new Vector3(960, 540, 0), Quaternion.Euler(new Vector3(0, 0, 0)), Draw.transform);
+        }
+        CG++;
+        allCG++;
+        PlayerPrefs.SetInt("抽卡次數", allCG);
+        Debug.Log(PlayerPrefs.GetInt("抽卡次數"));
+        //Debug.Log(allCG);
+    }
+}
---
+++ CardGet Script/Cardadd.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Cardadd : MonoBehaviour {
+    public Sprite S1;
+    public Sprite S2;
+    public Sprite S3;
+    public Button Cb1;
+    public Button Cb2;
+    public Button Cb3;
+    public GameObject CPre1;
+    public GameObject Cpre2;
+    public GameObject Cpre3;
+    public Button CD1;
+    public Button CD2;
+    public Button CD3;
+    public List<int> CN = new List<int>();
+    public int a;
+    public int save;
+    public int N1;
+    public int N2;
+    public int N3;
+    public Canvas CP;
+
+	// Use this for initialization
+	void Start () {
+
+        int A = 200;
+        save = PlayerPrefs.GetInt("牌組存檔");
+        N1 = 0;
+        N2 = 0;
+        N3 = 0;
+        Cb1.onClick.AddListener(delegate () {
+
+            if (N1 <= 2) {
+                CN.Add(1);
+                
+                Instantiate(CPre1, new Vector3(A, 830, 0), Quaternion.Euler(new Vector3(0, 0, 0)), CP.transform);
+                CPre1.transform.localPosition = new Vector3(0, 0, 0);
+      
+                Debug.Log(CN.Count);
+                a = CN.Count;
+                for (int i = 1; i < a; i++)
+                {
+                    Debug.Log(Cb1);
+                }
+                A += 300;
+                N1++;
+            }
+        });
+        Cb2.onClick.AddListener(delegate () {
+            if (N2 <= 2) {
+                CN.Add(2);
+                //Debug.Log(CN.Count);
+                a = CN.Count;
+                for (int i = 1; i < a; i++)
+                {
+                    Debug.Log(Cb2);
+                }
+                N2++;
+            }
+
+        });
+        Cb3.onClick.AddListener(delegate ()
+        {
+            if (N3 <= 2)
+            {
+                CN.Add(3);
+                Debug.Log(CN.Count);
+                a = CN.Count;
+                for (int i = 1; i < a; i++)
+                {
+                    Debug.Log(Cb3);
+                }
+                N3++;
+            }
+
+        });
+        CD1.onClick.AddListener(delegate ()
+        {
+            Debug.Log("XDDD");//沒有進來這一步
+            Destroy(CPre1);
+        });
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+    }
+}
